417e08c12f80c9920ef01186204df675403d263f
FilipeNV1 <filipeviseu@ua.pt>
2026-04-18 22:31:19 +0100

    only skip assignment if no stations available

--- tests/test_station_assignment.py
+++ tests/test_station_assignment.py
@@ -5,7 +5,6 @@ from threading import Event
 import paho.mqtt.client as mqtt
 import pytest
 
-pytestmark = pytest.mark.skip(reason="station assignment tests skipped")
 from helpers import (
     MQTT_HOST,
     MQTT_PORT,
